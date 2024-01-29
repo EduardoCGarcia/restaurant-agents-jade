7e4e4c2b88344146dd5d6e82323256fcac732ab6
EduardoCGarcia <echavezg004@gmail.com>
2024-01-29 14:37:41 -0600

    Serializacion del modelo Producto

--- src/main/java/com/edc/restaurant/models/Product.java
+++ src/main/java/com/edc/restaurant/models/Product.java
@@ -1,5 +1,6 @@
 package com.edc.restaurant.models;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -8,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Product {
+public class Product implements Serializable{
     private String nombre;
     private BigDecimal precio;
     private String descripcion;
