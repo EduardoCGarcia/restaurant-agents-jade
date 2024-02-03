78f7fe39aec5efdda43789adadbfff647891ad95
EduardoCGarcia <echavezg004@gmail.com>
2024-02-02 21:31:52 -0600

    nuevo metodo para lectura del archivo de configuración

--- src/main/java/com/edc/restaurant/data/IData.java
+++ src/main/java/com/edc/restaurant/data/IData.java
@@ -1,7 +1,9 @@
 package com.edc.restaurant.data;
 
+import com.edc.restaurant.models.ConfigFile;
 import java.util.ArrayList;
 
 public interface IData {
     public ArrayList readData(String fileName);
+    public ConfigFile readConfigFile(String path);
 }
